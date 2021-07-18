9d7b172c319356e3394a82c5b0b49b272b8b71d0
luizedu-ms <luedu159@gmail.com>
2021-07-18 15:20:06 -0300

    add footer e outros func no script

--- components/formulario.js
+++ components/formulario.js
@@ -1,6 +1,42 @@
 const form = document.getElementById('form');
 
+
 form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let name =  document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    
+    let data = {
+        name,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+    localStorage.setItem('lead', convertData);
+
+    let content = document.getElementById('modal-content');
+    let modal = document.getElementById('modalId');
+    let loading = `<p> Carregando...</p>`;
+    let done = `<p>Concluido, mais detalhes em seu e-mail!!</p>`;
+
+    content.style.visibility = "visible";
+    modal.innerHTML = loading;
+    
+    setTimeout(() => {
+        modal.innerHTML = done;
+        document.getElementById('name').value = '';
+        document.getElementById('email').value = '';
+    },1000);
+
+    setTimeout(() => {
+        content.innerHTML = '';
+        content.style.visibility = "hidden";
+        document.location.reload();
+    },3000);
+});
+
+
+const formFooter = document.getElementById('form-footer');
+formFooter.addEventListener("submit", (e) => {
     e.preventDefault();
     let name =  document.getElementById('name').value;
     let email = document.getElementById('email').value;
